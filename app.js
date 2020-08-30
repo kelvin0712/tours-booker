9a01b55631383a7a92acaa16b256efbafe2fcf5f
Kelvin <duchung97dl@gmail.com>
2020-08-31 09:57:32 +1000

    Refactor routes.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -17,9 +17,9 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -37,9 +37,9 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getOneTour = (req, res) => {
   const id = req.params.id * 1;
 
   const tour = tours.find((el) => el.id === id);
@@ -58,9 +58,22 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
+  if (!req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+const updateTour = (req, res) => {
   if (!req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -73,20 +86,15 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '',
     },
   });
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
-  if (!req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-});
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getOneTour)
+  .delete(deleteTour)
+  .patch(updateTour);
 
 const port = 3000;
 app.listen(port, () => {
