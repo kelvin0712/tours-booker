1d4518a721d43164417364bfbbf7580768c875bf
Kelvin <duchung97dl@gmail.com>
2020-09-04 10:18:31 +1000

    Create connection with Mongo, refactor files structure to MVC model.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,21 +1,8 @@
-const fs = require('fs');
+const Tour = require('../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkId = (req, res, next, val) => {
-  console.log(`this is the id: ${val}`);
-
-  if (!req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
-
-  next();
-};
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
 
 exports.checkValidTour = (req, res, next) => {
   const { name, price } = req.body;
@@ -41,23 +28,12 @@ exports.getAllTours = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tours: newTour,
-        },
-      });
-    }
-  );
+  // res.status(201).json({
+  //   status: 'success',
+  //   data: {
+  //     tours: newTour,
+  //   },
+  // });
 };
 
 exports.getOneTour = (req, res) => {
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successfully!');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
