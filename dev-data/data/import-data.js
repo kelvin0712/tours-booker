2eb31e1b9202fbc2919ec955cf4c63c5273be124
Kelvin <duchung97dl@gmail.com>
2020-09-06 15:42:20 +1000

    Build a script to import data automatically.

---
+++ dev-data/data/import-data.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successfully!');
+  });
+
+// Read json file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Import data into database
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// Delete all data from collections
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
