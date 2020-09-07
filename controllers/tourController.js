6010221d1cc16c3bf15cb0f023f60310f0ff9ee7
Kelvin <duchung97dl@gmail.com>
2020-09-07 10:46:06 +1000

    Implementing pagination, alias to the Tour API.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -17,6 +17,14 @@ const Tour = require('../models/tourModel');
 //   next();
 // };
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingAverage,price';
+  req.query.fields = 'name,price,ratingAverage,summary,diffuculty';
+
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // Build query
@@ -31,13 +39,33 @@ exports.getAllTours = async (req, res) => {
 
     let query = Tour.find(JSON.parse(queryStr));
 
-    // 3.Sorting
+    // 2.Sorting
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
       // sort('price rating')
     } else {
       query = query.sort('-createAt');
+    }
+
+    // 3.Fields Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4. Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist!');
     }
 
     // Execute query
